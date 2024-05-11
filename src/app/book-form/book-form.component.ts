151e5d233d7fdd9600cb9dadfa974f0d63ed41aa
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-05-11 13:07:13 -0500

    manejo de errores

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: []
 })
 export class BookFormComponent {
+
+    errors:string[] = [];
+
     form: FormGroup = this.fb.group({
       title:['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
       slug:['', [Validators.required, Validators.pattern('[A-Za-z0-9-]+')]],
@@ -36,8 +39,21 @@ export class BookFormComponent {
       book.filePath = 'dummy.pdf';
 
       this.bookService.create(book)
-        .subscribe(book =>{
-          this.router.navigate(['/']);
+        .subscribe({
+
+          next:book =>{
+            this.router.navigate(['/']);
+          },
+
+          error: error =>{
+
+            if(error.error.status === 400){
+               this.errors.push(error.error.detail)
+            } else if( error.error.status === 422){
+                this.errors.push(...error.error.errors)
+            }
+          }
+
         });
     }
 }
