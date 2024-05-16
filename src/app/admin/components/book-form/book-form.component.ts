415525c0e6e7220b8d1da505babf320439ef0dee
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-05-16 15:24:01 -0500

    automatizando slug, en la clases creando metodo generarteSlug(), agregandolo en el form

--- src/app/admin/components/book-form/book-form.component.ts
+++ src/app/admin/components/book-form/book-form.component.ts
@@ -44,7 +44,7 @@ export class BookFormComponent implements OnInit{
           this.form = this.fb.group({
             title:[, [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
             slug:[, [Validators.required, Validators.pattern('[A-Za-z0-9-]+')]],
-            desc:[, [Validators.required]],
+            desc:[, [Validators.required, Validators.maxLength(100)]],
             price:[, [Validators.required, Validators.min(0)]],
             coverPath:[, [Validators.required]],
             filePath:[, [Validators.required]]
@@ -69,7 +69,18 @@ export class BookFormComponent implements OnInit{
             this.form!.controls[control].setValue(response.path);
           });
       }
+    }
 
+    generateSlug(){
+      const slug = this.form!.controls['title'].value
+      .toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
+
+      this.form!.controls['slug'].setValue(slug);
     }
 
     save(){
