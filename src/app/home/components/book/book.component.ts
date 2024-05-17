e3945e6e92e411fcb9293c4542a09b11d23d75f7
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-05-17 14:43:48 -0500

    agregando funciones de agregar y eliminar en los detalles del libro, tambien se ajusto el tamaño de las imagnes para que no se desbordaran

--- src/app/home/components/book/book.component.ts
+++ src/app/home/components/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../../services/home.service';
 import { Book } from 'src/app/interfaces/book.interface';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-book',
@@ -15,6 +16,7 @@ export class BookComponent implements OnInit{
   constructor(
     private homeService:HomeService,
     private route: ActivatedRoute,
+    private cartService: CartService
   ){}
 
   ngOnInit(): void {
@@ -24,6 +26,19 @@ export class BookComponent implements OnInit{
     .subscribe(book=>{
         this.book = book;
     })
+  }
+
+  
+  addBookToCart(){
+    this.cartService.addItem(this.book!);
+  }
+
+  removeBookFromCart(){
+    this.cartService.removeItem(this.book!);
+  }
+
+  bookIsInCart(){
+    return this.cartService.itemAlreadyExists(this.book!);
   }
 
 }
