648196a1b4d2af63601cda8c4505cf53e77b2323
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-05-17 13:43:45 -0500

    crear cart.service, implemnter meetodos agrregar remvoer limpiar consulatar si existe y guardar local storage, clase llamar los metodos, y en el html poner esos eventeos en los botones

--- src/app/home/components/layout/layout.component.ts
+++ src/app/home/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class LayoutComponent {
+
+  constructor(
+    private cartService:CartService
+  ){}
 
 }
--- src/app/home/components/shared/book-card/book-card.component.ts
+++ src/app/home/components/shared/book-card/book-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Book } from 'src/app/interfaces/book.interface';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-book-card',
@@ -8,6 +9,22 @@ import { Book } from 'src/app/interfaces/book.interface';
 })
 export class BookCardComponent {
   @Input(/*{requiered: true}*/) book!: Book; //quiete el requierd por que me generaba conflicto
+
+  constructor(
+    private cartService:CartService
+  ){}
+
+  addBookToCart(){
+    this.cartService.addItem(this.book);
+  }
+
+  removeBookFromCart(){
+    this.cartService.removeItem(this.book);
+  }
+
+  bookIsInCart(){
+    return this.cartService.itemAlreadyExists(this.book);
+  }
 
 }
 
